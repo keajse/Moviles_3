e3a0940099794a2731ad23d38512d645f29109a6
Laura Serrano _ Keajse <lajusega@gmail.com>
2021-03-20 09:54:58 -0500

    Real

--- momentoUnoM3.js
+++ momentoUnoM3.js
@@ -34,24 +34,30 @@ for (let x = 1; x <= numPeople; x++) {
     
     let cant = prompt("Cantidad: ");
 
-    if (plato = "1") {
-       totalPersona = cant * 10000;
+    if (plato ==1) {
+        burguer = cant * 10000;
         
     }
-    else if (plato = "2") {
-        totalPersona = cant * 8000;
+    else if (plato == 2) {
+        dogs = cant * 8000;
         
     }
-    else if (plato = "3") {
-        totalPersona = cant * 6000;
+    else if (plato == 3) {
+        salchipapas = cant * 6000;
         
     }
-    else if (plato = "4") {
-        totalPersona = cant * 7000;
+    else if (plato ==4) {
+        chorizos = cant * 7000;
         
     }
 
-       
+    totalBurguer= totalBurguer+ burguer
+    totalDogs= totalDogs + dogs
+    totalSalchipapas = totalSalchipapas + salchipapas
+    totalChorizos = totalChorizos + chorizos
+
+    totalPersona = totalBurguer + totalDogs + totalSalchipapas + totalChorizos;
+    
     subtotal = subtotal + totalPersona
     
 }
@@ -60,7 +66,7 @@ for (let x = 1; x <= numPeople; x++) {
 console.log(`El Subtotal es: ${subtotal}`)
 
 if (subtotal > 20000) {
-    descuento = subtotal * 0.10
+    descuento = subtotal * 10/100
     subtotalDescuento = subtotal - descuento;
 
     console.log(`Por su compra mayor a $20.000 ha obtenido un descuento del 10% es decir: ${descuento}`)
@@ -74,15 +80,17 @@ else {
 let rtaPropina = prompt("¿Desea incluir propina? 1. si o 2. no")
 console.log(rtaPropina)
 
-if (rtaPropina = "1") {
-    propina = subtotalDescuento * 0.10
-    totalPropina = subtotalDescuento + propina
+if (rtaPropina == "1") {
+    propina = subtotalDescuento * 10 / 100
+    
 }
 
 else {
-    propina = subtotalDescuento * 0
-    totalPropina = subtotalDescuento + propina
+    propina = subtotalDescuento * 0 /100
+    totalPropina = subtotalDescuento
 }
+
+totalPropina = subtotalDescuento + propina
 
 console.log(`El total a pagar es: ${totalPropina}`)
 
